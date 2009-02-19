ce1a023e302405377b012f8f46de2cd0d2c912f2
mike <mike@cordax.net>
2009-02-19 01:18:51 -0800

    updated company name

--- main.m
+++ main.m
@@ -3,7 +3,7 @@
 //  InitiativeTracker
 //
 //  Created by Mike Pattee on 1/25/09.
-//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//  Copyright Cordax Software, LLC 2009. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>

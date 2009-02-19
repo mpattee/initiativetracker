4f45bc3d7ea831c1ac67e2458723d6cda4b8c456
mike <mike@cordax.net>
2009-02-19 00:24:15 -0800

    updated company name

--- InitiativeTrackerController.m
+++ InitiativeTrackerController.m
@@ -3,7 +3,7 @@
 //  InitiativeTracker
 //
 //  Created by Mike Pattee on 1/27/09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//  Copyright 2009 Cordax Software, LLC. All rights reserved.
 //
 
 #import "InitiativeTrackerController.h"
